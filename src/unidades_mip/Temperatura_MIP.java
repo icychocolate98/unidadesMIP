26cd19261f9a46fbffec35fb2332d58c27fb5bdd
Ivan Novoa <daw117@ACARBALLEIRA.LOCAL>
2019-05-29 11:14:09 +0200

    Metodo celsiusAFahrenheit creado

--- src/unidades_mip/Temperatura_MIP.java
+++ src/unidades_mip/Temperatura_MIP.java
@@ -6,4 +6,9 @@ package unidades_mip;
  */
 public class Temperatura_MIP extends UnidadesApp_MIP{
     
+    public static double celsiusAFahrenheit(double celsius) {
+        fahrenheit = (celsius * 9/5) + 32;
+        return fahrenheit;
+    }
+    
 }
