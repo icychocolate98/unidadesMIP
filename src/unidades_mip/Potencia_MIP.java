1f3ba8b03ed9cec7392a381a34ddd0cc240e1503
Ivan Novoa <DAW117@acarballeira.local>
2019-05-29 10:58:10 +0200

    Metodo kwACv creado

---
+++ src/unidades_mip/Potencia_MIP.java
@@ -0,0 +1,14 @@
+package unidades_mip;
+/**
+ *
+ * @author daw117
+ */
+public class Potencia_MIP extends UnidadesApp_MIP{
+    
+    public static double kwACv(double kw) {
+        cv = kw * 1.34102;
+        return cv;
+    }
+
+    
+}
