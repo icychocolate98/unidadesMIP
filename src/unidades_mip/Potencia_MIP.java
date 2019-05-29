8d59837905e535c0b896244e21e2166a7294b327
Ivan Novoa <DAW117@acarballeira.local>
2019-05-29 10:58:54 +0200

    Metodo cvAKw creado

--- src/unidades_mip/Potencia_MIP.java
+++ src/unidades_mip/Potencia_MIP.java
@@ -10,5 +10,10 @@ public class Potencia_MIP extends UnidadesApp_MIP{
         return cv;
     }
 
+    public static double cvAKw(double cv) {
+        kw = cv / 1.341;
+        return kw;
+    }
+
     
 }
