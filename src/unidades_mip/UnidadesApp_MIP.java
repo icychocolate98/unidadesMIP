9f60d7918df59f34c8624eb2cf8e732eab5d50d9
daw116 <pablo99viana@hotmail.com>
2019-05-29 11:04:25 +0200

    menu principal creado

--- src/unidades_mip/UnidadesApp_MIP.java
+++ src/unidades_mip/UnidadesApp_MIP.java
@@ -1,21 +1,53 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// Clase Principal UnidadesAPP_MIP - Pablo
 package unidades_mip;
+
+import java.util.Scanner;
 
 /**
  *
- * @author daw111
+ * @author daw116
  */
 public class UnidadesApp_MIP {
+
+    Scanner leer = new Scanner(System.in);
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
+        int op = 0;
+
+        do {
+            op = mostrarMenuPrincipal();
+            switch (op) {
+
+                case 1:
+                    //menuTransformarLongitud();
+                    break;
+                case 2:
+                    //menuTransformarPotencia();
+                    break;
+                case 3:
+                    //menuTransformarTemperatura();
+                    break;
+                case 4:
+                    break;
+            }
+
+        } while (op !=4);
     }
-    
+
+    public static int mostrarMenuPrincipal() {
+        int op = 0;
+        final int SALIR = 4;
+
+        System.out.println("1.-Transformar Longitud");
+        System.out.println("2.-Transformar Potencia");
+        System.out.println("3.-Transformar Temperatura");
+        System.out.println("4.- SALIR");
+        return op;
+    }
 }
+
